b88b3d93c050dc7935ba70323c4c6c6f89f69403
bereketsintayehu <bereketsintayehu32@gmail.com>
2024-04-19 10:41:36 +0300

    mongo url fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { ConfigModule } from '@nestjs/config';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-console.log(`${process.env.MONGOUSER}`)
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
